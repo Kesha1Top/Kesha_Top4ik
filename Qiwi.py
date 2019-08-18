e724e49ba18a58346bc6cb2d2cc9079d847b168d
Kesha1Top <54248261+Kesha1Top@users.noreply.github.com>
2019-08-18 22:50:09 +0300

    Create Qiwi.py

---
+++ Qiwi.py
@@ -0,0 +1,30 @@
+from SimpleQIWI import *
+
+token=input('Токен: ')
+phone=input('Номер кошелька: ')
+api = QApi(token=token, phone=phone)
+
+print('Выберите функцию: ')
+print('[1] - Баланс счета')
+print('[2] - Вывод денег')
+vvod = input("")
+
+def info():
+print('Кошелек найден!')
+print('Баланс клиента с номером', phone, 'равен: ', api.balance)
+
+def vanish():
+summ = input('Введите сумму: ')
+num = input('Номер кошелька: ')
+try:
+api.pay(account = str(num), amount = int(summ), comment = ' ') #Вывод денег,
+print('Баланс клиента с номером', phone, 'равен: ', api.balance)
+except QIWIAPIError:
+print("Ошибка 1. Невозможно осуществить платеж самому себе.")
+
+if vvod == "1":
+info()
+elif vvod == "2":
+vanish()
+else:
+quit
